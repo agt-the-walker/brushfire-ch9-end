8bac7cdc985336671f0c4ed2d05422efcc4703bb
Jerome Zago <jerome.zago@gmail.com>
2017-06-07 12:12:07 +0200

    More explicit routes

--- config/routes.js
+++ config/routes.js
@@ -6,12 +6,25 @@ module.exports.routes = {
 
   'PUT /login': 'UserController.login',
   'GET /logout': 'UserController.logout',
+  'GET /video': 'VideoController.find',
+  'POST /video': 'VideoController.create',
+
+  'POST /user/signup': 'UserController.signup',
+  'PUT /user/removeProfile': 'UserController.removeProfile',
+  'PUT /user/restoreProfile': 'UserController.restoreProfile',
+  'PUT /user/restoreGravatarURL': 'UserController.restoreGravatarURL',
+  'PUT /user/updateProfile/:id': 'UserController.updateProfile',
+  'PUT /user/changePassword': 'UserController.changePassword',
+  'GET /user/adminUsers': 'UserController.adminUsers',
+  'PUT /user/updateAdmin/:id': 'UserController.updateAdmin',
+  'PUT /user/updateBanned/:id': 'UserController.updateBanned',
+  'PUT /user/updateDeleted/:id': 'UserController.updateDeleted',
 
   /*************************************************************
    * Server-rendered HTML Pages                                *
    *************************************************************/
 
-  'GET /': 'PageController.showHomePage',  
+  'GET /': 'PageController.showHomePage',
 
   'GET /videos': 'PageController.showVideosPage',
 
